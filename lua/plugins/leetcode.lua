b1d8781fffcb312448cc37c58093f0cad04af500
Mykola Vaskevych <22372199@studentmail.ul.ie>
2025-07-24 03:12:59 +0200

    proper leet group in whichkey

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -7,57 +7,48 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     "nvim-tree/nvim-web-devicons",
-    "3rd/image.nvim",
   },
   opts = {
     lang = "python3",
     image_support = true,
     logging = false,
   },
-
   keys = {
-    -- Main
-    { "<leader>lm", "<cmd>Leet menu<cr>", desc = "Menu" },
-    { "<leader>ll", "<cmd>Leet list<cr>", desc = "List problems" },
-    { "<leader>ld", "<cmd>Leet daily<cr>", desc = "Daily challenge" },
-    { "<leader>lr", "<cmd>Leet random<cr>", desc = "Random problem" },
-    { "<leader>lt", "<cmd>Leet tabs<cr>", desc = "Switch tabs" },
+    -- Primary actions (most used - single keys)
+    { "<leader>ll", "<cmd>Leet<cr>", desc = "Menu (Dashboard)" },
+    { "<leader>lr", "<cmd>Leet run<cr>", desc = "Run code" },
+    { "<leader>ls", "<cmd>Leet submit<cr>", desc = "Submit solution" },
+    { "<leader>lt", "<cmd>Leet test<cr>", desc = "Test code" },
+    { "<leader>lq", "<cmd>Leet exit<cr>", desc = "Quit Leetcode" },
 
-    -- Execute
-    { "<leader>lR", "<cmd>Leet run<cr>", desc = "Run" },
-    { "<leader>ls", "<cmd>Leet submit<cr>", desc = "Submit" },
+    -- Problem navigation
+    { "<leader>lnl", "<cmd>Leet list<cr>", desc = "List all" },
+    { "<leader>lnd", "<cmd>Leet daily<cr>", desc = "Daily challenge" },
+    { "<leader>lnr", "<cmd>Leet random<cr>", desc = "Random problem" },
+    { "<leader>lnt", "<cmd>Leet tabs<cr>", desc = "Switch tabs" },
 
-    -- Code
-    { "<leader>ly", "<cmd>Leet yank<cr>", desc = "Yank code" },
-    { "<leader>lx", "<cmd>Leet reset<cr>", desc = "Reset code" },
-    { "<leader>lL", "<cmd>Leet last_submit<cr>", desc = "Last submission" },
-
-    -- UI
-    { "<leader>lc", "<cmd>Leet console<cr>", desc = "Console" },
-    { "<leader>li", "<cmd>Leet info<cr>", desc = "Info" },
-    { "<leader>lo", "<cmd>Leet open<cr>", desc = "Open in browser" },
-    { "<leader>le", "<cmd>Leet exit<cr>", desc = "Exit" },
-
-    -- Filters
-    { "<leader>lfe", "<cmd>Leet list difficulty=Easy<cr>", desc = "Easy" },
-    { "<leader>lfm", "<cmd>Leet list difficulty=Medium<cr>", desc = "Medium" },
-    { "<leader>lfh", "<cmd>Leet list difficulty=Hard<cr>", desc = "Hard" },
+    -- Filtered lists
+    { "<leader>lfe", "<cmd>Leet list difficulty=Easy<cr>", desc = "Easy problems" },
+    { "<leader>lfm", "<cmd>Leet list difficulty=Medium<cr>", desc = "Medium problems" },
+    { "<leader>lfh", "<cmd>Leet list difficulty=Hard<cr>", desc = "Hard problems" },
     { "<leader>lfa", "<cmd>Leet list status=AC<cr>", desc = "Accepted" },
+    { "<leader>lfn", "<cmd>Leet list status=NOT_STARTED<cr>", desc = "Not started" },
 
-    -- Random by difficulty
-    { "<leader>lre", "<cmd>Leet random difficulty=Easy<cr>", desc = "Random easy" },
-    { "<leader>lrm", "<cmd>Leet random difficulty=Medium<cr>", desc = "Random medium" },
-    { "<leader>lrh", "<cmd>Leet random difficulty=Hard<cr>", desc = "Random hard" },
+    -- Random with difficulty
+    { "<leader>lae", "<cmd>Leet random difficulty=Easy<cr>", desc = "Random easy" },
+    { "<leader>lam", "<cmd>Leet random difficulty=Medium<cr>", desc = "Random medium" },
+    { "<leader>lah", "<cmd>Leet random difficulty=Hard<cr>", desc = "Random hard" },
+
+    -- Code management
+    { "<leader>lcy", "<cmd>Leet yank<cr>", desc = "Yank code" },
+    { "<leader>lcr", "<cmd>Leet reset<cr>", desc = "Reset to default" },
+    { "<leader>lcl", "<cmd>Leet last_submit<cr>", desc = "Load last submission" },
+    { "<leader>lcL", "<cmd>Leet lang<cr>", desc = "Change language" },
+
+    -- UI/View
+    { "<leader>lvc", "<cmd>Leet console<cr>", desc = "Toggle console" },
+    { "<leader>lvd", "<cmd>Leet desc<cr>", desc = "Toggle description" },
+    { "<leader>lvi", "<cmd>Leet info<cr>", desc = "Problem info" },
+    { "<leader>lvo", "<cmd>Leet open<cr>", desc = "Open in browser" },
   },
-
-  config = function(_, opts)
-    require("leetcode").setup(opts)
-
-    local wk = require("which-key")
-    wk.add({
-      { "<leader>l", group = "Leetcode" },
-      { "<leader>lf", group = "Filter" },
-      { "<leader>lr", group = "Random" },
-    })
-  end,
 }
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/which-key.nvim",
+  optional = true,
+  opts = {
+    spec = {
+      { "<leader>l", group = "leetcode", icon = { icon = "󰖟", color = "orange" } },
+      { "<leader>ln", group = "navigate", icon = { icon = "", color = "blue" } },
+      { "<leader>lf", group = "filter", icon = { icon = "", color = "green" } },
+      { "<leader>la", group = "random level", icon = { icon = "󰒲", color = "purple" } },
+      { "<leader>lc", group = "code", icon = { icon = "", color = "cyan" } },
+      { "<leader>lv", group = "view", icon = { icon = "", color = "yellow" } },
+    },
+  },
+}
