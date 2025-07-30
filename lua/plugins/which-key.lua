8768fc588093c0785d94bc41998c816001bf4149
Mykola Vaskevych <22372199@studentmail.ul.ie>
2025-07-30 05:23:46 +0200

    update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,9 +3,10 @@ return {
   optional = true,
   opts = {
     spec = {
+      -- leetcode group with subs
       { "<leader>l", group = "leetcode", icon = { icon = "󰖟", color = "orange" } },
       { "<leader>ln", group = "navigate", icon = { icon = "", color = "blue" } },
-      { "<leader>lf", group = "filter", icon = { icon = "", color = "green" } },
+      { "<leader>lf", group = "filter", icon = { icon = "🔍", color = "green" } },
       { "<leader>la", group = "random level", icon = { icon = "󰒲", color = "purple" } },
       { "<leader>lc", group = "code", icon = { icon = "", color = "cyan" } },
       { "<leader>lv", group = "view", icon = { icon = "", color = "yellow" } },
