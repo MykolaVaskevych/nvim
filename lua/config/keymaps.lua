8f3b43a190202e73fb8ae0c73e85f56656f8e102
mykola vaskevych <bebagi61@gmail.com>
2025-08-09 22:24:13 +0100

    jj to jo

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "jo", "<ESC>", { silent = true })
 vim.keymap.del("n", "<leader>l")
 vim.keymap.del("n", "<leader>L")
